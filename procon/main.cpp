ec5440f7bad39fcbcd2ff57f1647a7d45c1c88b4
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2015-04-17 15:34:45 +0900

    Initial Commit

---
+++ procon/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  procon
+//
+//  Created by 坂本　祥之　 on 2015/04/17.
+//  Copyright (c) 2015年 坂本　祥之　. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
